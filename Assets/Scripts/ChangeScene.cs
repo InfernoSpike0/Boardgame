38697279e9d42716b40235eea8aa621baff1237d
Alexander <xrdharley@gmail.com>
2025-09-22 16:35:33 -0400

    Added main menu
    There isn't much, but it exists

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void ChangeBoard()
+    {
+        SceneManager.LoadScene("Board");
+    }
+    public void ChangeMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void ChangePass()
+    {
+        SceneManager.LoadScene("PassTurn");
+    }
+    public void ChangeScores()
+    {
+        SceneManager.LoadScene("Scores");
+    }
+}
