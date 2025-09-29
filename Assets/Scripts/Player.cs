25a05eca73dc2c9f6681d100d27dc5b4d66b6a3c
Milkhead5 <69921044+Milkhead5@users.noreply.github.com>
2025-09-28 20:22:44 -0400

    Color ID's for players + Paths/nodes clickable with collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
 using System.Collections.Generic;
-using System.Collections;
 
-public class Player : MonoBehaviour
+[System.Serializable]   // lets you view/edit in Inspector if stored in GameBase
+public class Player
 {
-    public int playerNumber;
-    public int score;
-    public Dictionary<string, int> currencies;
+    public int id;                // Unique ID 
+    public int playerNumber;      // Turn order number
+    public int score;             // Turrent score
+    public Color color;           // Player color
+    public Dictionary<string, int> currencies; // In-game currencies
 
-    public Player(int number)
+    public Player(int id, Color color, int number = 0)
     {
-        playerNumber = number;
-        score = 0;
+        this.id = id;
+        this.color = color;
+        this.playerNumber = number == 0 ? id : number;
+        this.score = 0;
+
         currencies = new Dictionary<string, int>
         {
             { "money1", 0 },
@@ -19,12 +24,20 @@ public class Player : MonoBehaviour
             { "money3", 0 },
             { "money4", 0 },
             { "money5", 0 },
-            { "joker", 0}
+            { "joker", 0 }
         };
     }
 
     public void AddCurrency(string currency, int amount)
     {
+        if (!currencies.ContainsKey(currency))
+            currencies[currency] = 0;
+
         currencies[currency] += amount;
+    }
+
+    public override string ToString()
+    {
+        return $"Player {id} (Color: {color}, Score: {score})";
     }
 }
